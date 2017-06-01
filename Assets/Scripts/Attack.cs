3957370e339a560c5dfdfc2aef89e4e3eb151aa0
brandonbrule <brandonbrule@gmail.com>
2017-05-31 20:43:46 -0400

    I think all the assets to start.

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+    public GameObject Player;
+    public GameObject PlayerActiveItem;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.E)) //set the key you want to be pressed
+        {
+
+            PlayerActiveItem.SetActive(false);
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other);
+        if (other.gameObject.CompareTag("Chest"))
+        {
+            Debug.Log("test");
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControls.js
@@ -0,0 +1,71 @@
+﻿ #pragma strict
+ 
+ 
+ 
+ //Inspector Variables
+ var playerSpeed : float = 10; //speed player moves
+ var turnSpeed : float = 100; // speed player turns
+
+ //var animObj : GameObject = GameObject.Find("/Player/sword"); //set the game object you want to be animated
+ var sword : GameObject;
+// This returns the GameObject named Hand in the Scene.
+	sword = GameObject.Find("sword");
+ 
+ function Update () 
+ {
+ 
+     MoveForward(); // Player Movement
+     TurnRightAndLeft();//Player Turning
+	 Run();
+	 Attack();
+
+ }
+
+ function Attack()
+ {
+	if (Input.GetKeyDown(KeyCode.E)) //set the key you want to be pressed
+     {
+	 //Debug.Log(sword);
+     }
+ }
+
+ function Run()
+ {
+	if(Input.GetKeyDown(KeyCode.LeftShift)){
+		playerSpeed = 40;
+	}
+
+	if(Input.GetKeyUp(KeyCode.LeftShift)){
+		playerSpeed = 10;
+	}
+ }
+ 
+ function MoveForward()
+ {
+ 
+     if(Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
+     {
+         transform.Translate(0,playerSpeed * Time.deltaTime,0);
+     }
+
+	 if(Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
+     {
+         transform.Translate(0,-playerSpeed * Time.deltaTime,0);
+     }
+ 
+ }
+ 
+ function TurnRightAndLeft()
+ {
+ 
+     if(Input.GetKey("right")) //Right arrow key to turn right
+     {
+         transform.Rotate(-Vector3.forward *turnSpeed* Time.deltaTime);
+     }
+ 
+     if(Input.GetKey("left"))//Left arrow key to turn left
+     {
+         transform.Rotate(Vector3.forward *turnSpeed* Time.deltaTime);
+     }
+ 
+ }
